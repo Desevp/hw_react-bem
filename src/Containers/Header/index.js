4669805e6c80913b8f67e9428039bf89bc4ca5cb
Jane Des <desevp@gmail.com>
2018-11-10 23:29:40 +0300

    Added scss

--- src/Containers/Header/index.js
+++ src/Containers/Header/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import './style.scss';
 
 const Header = () => (
   <div className="header">
-    Header
+    <div className="header__test">
+      Header
+    </div>
   </div>
 );
 
